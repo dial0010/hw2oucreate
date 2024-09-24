3bd0c003837af8746853e93667d3f1b0c274bfac
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-09-24 11:54:44 -0500

    Create post-result.php

---
+++ post-result.php
@@ -0,0 +1,18 @@
+<?php
+$pageTitle = "Post result";
+include "view-header.php";
+?>
+    <h1>Post result</h1>
+<?php
+echo getDisplay();
+include "view-footer.php";
+
+function getDisplay() {
+    if (isset($_POST['my-name'])) {
+        return "<p>The value sent is:</p>" . $_POST['my-name'];
+} else {
+  return "<p>Nothing posted to the page.</p>";
+<?php
+} 
+}
+?>
