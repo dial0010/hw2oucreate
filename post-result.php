900cd4b98368a2e265691aae80bfd290d82f0e24
dial0010 <158079380+dial0010@users.noreply.github.com>
2024-09-26 16:13:58 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,7 +2,7 @@
 $pageTitle = "Post result";
 include "view-header.php";
 ?>
-    <h1>Post result</h1>
+<h1>Post result</h1>
 <?php
 echo getDisplay();
 include "view-footer.php";
@@ -10,9 +10,8 @@ include "view-footer.php";
 function getDisplay() {
     if (isset($_POST['my-name'])) {
         return "<p>The value sent is:</p>" . $_POST['my-name'];
-} else {
-  return "<p>Nothing posted to the page.</p>";
-<?php
-} 
+    } else {
+        return "<p>Nothing posted to the page.</p>";
+    }  // Correctly close the function here
 }
 ?>
